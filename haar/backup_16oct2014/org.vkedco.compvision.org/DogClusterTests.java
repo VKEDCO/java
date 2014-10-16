d69b2aa22e008dd1dd420a90782fa3e6803536e4
vkedco <vladimir.kulyukin@gmail.com>
2014-10-16 14:06:38 -0600

    Create DogClusterTests.java

---
+++ haar/backup_16oct2014/org.vkedco.compvision.org/DogClusterTests.java
@@ -0,0 +1,17 @@
+package org.vkedco.compvision.dog;
+
+import java.util.ArrayList;
+
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.vkedco.opencv.haar.HaarWaveletWin;
+import org.vkedco.opencv.haar.TwoDHaarCluster;
+import org.vkedco.opencv.haar.TwoDMatHaar;
+
+public class DogClusterTests {
+	
+	static final String IMG_PATH2 = "/home/vladimir/programming/opencv/images/BlurImages/";
+	
+	
+
+}
