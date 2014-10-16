b3c534bdbb24dc1b8170e9c82260db25aaa817ec
vkedco <vladimir.kulyukin@gmail.com>
2014-10-16 14:07:00 -0600

    Create DOGConstants.java

---
+++ haar/backup_16oct2014/org.vkedco.compvision.org/DOGConstants.java
@@ -0,0 +1,15 @@
+package org.vkedco.compvision.dog;
+
+public class DOGConstants {
+
+	public static final int RECT_SIZE=20;	
+	public static final double THETA_THRESH	= 360;
+	public static final double MAGN_THRESH	= 20;
+	public static final double FREQ_THRESH = 0.02;
+	public static final double SIM_THETA_THRESH = 5;
+	public static final double CANNY_THRESH_1 = 200;
+	public static final double CANNY_THRESH_2 = 300;
+	public static final int NUMBER_OF_SCANLINES = 10;
+	public static final int LINE_GROWER_LIMIT = 15;
+	public static final String PATH="/home/vladimir/programming/opencv/images/BlurImages/";
+}
