a175efd90b7cc95a982bf73933acbbab4a88a953
vkedco <vladimir.kulyukin@gmail.com>
2015-07-17 13:31:45 -0600

    Create F_ex17_p79.java

---
+++ stewarts_calc/org.vkedco.calc.utils/org.vkedco.calc.chapter_01/F_ex17_p79.java
@@ -0,0 +1,17 @@
+package org.vkedco.calc.chapter_01;
+
+import org.vkedco.calc.utils.Function;
+
+/**
+ *
+ * @author vladimir kulyukin
+ */
+public class F_ex17_p79 extends Function {
+    
+    public F_ex17_p79() {}
+    @Override
+    public double v(double x) {
+        return 4 - (3.0/5.0)*x;
+    }
+    
+}
