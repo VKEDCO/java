baea56c4dc34f2a57b67af92cf4d83efa2710755
vkedco <vladimir.kulyukin@gmail.com>
2015-07-17 13:31:11 -0600

    Create Example02_p74.java

---
+++ stewarts_calc/org.vkedco.calc.utils/org.vkedco.calc.chapter_01/Example02_p74.java
@@ -0,0 +1,16 @@
+package org.vkedco.calc.chapter_01;
+
+import org.vkedco.calc.utils.Function;
+
+/**
+ *
+ * @author vladimir
+ */
+public class Example02_p74 extends Function {
+    
+    public Example02_p74() {}
+    @Override
+    public double v(double x) {
+        return 4*x - 5;
+    }
+}
