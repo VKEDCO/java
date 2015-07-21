35b472c3e716e800e497e2b99f7cf7fd59761412
vkedco <vladimir.kulyukin@gmail.com>
2015-07-21 13:35:53 -0600

    Create Partition.java

---
+++ tolstov/org.vkedco.tolstov.utils/Partition.java
@@ -0,0 +1,23 @@
+package org.vkedco.tolstov.utils;
+
+/**
+ *
+ * @author vladimir kulyukin
+ */
+public class Partition {
+    
+    public static double[] partition(double from, double upto, double step) {
+        if ( upto <= from ) return null;
+        int n = (int)((upto - from)/step) + 1;
+        double[] interval = new double[n];
+       
+        //System.out.println("n=" + n);
+        int i;
+        double curr;
+        for(i = 0, curr = from; i < n; i++, curr += step) {
+            //System.out.println("i=" + i + " " + "curr=" + curr);
+            interval[i] = curr;
+        }
+        return interval;
+    }
+}
