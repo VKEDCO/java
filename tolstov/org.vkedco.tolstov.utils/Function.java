a84788121158fbb9c21dfe92ab6977a5753c0b7e
vkedco <vladimir.kulyukin@gmail.com>
2015-07-21 13:34:45 -0600

    Create Function.java

---
+++ tolstov/org.vkedco.tolstov.utils/Function.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author vladimir kulyukin
+ */
+package org.vkedco.tolstov.utils;
+
+public class Function {
+    
+    public Function() {}
+    public double v(double x) { return 0; }
+    
+    public double[] generateRangeInterval(double[] domain_values) {
+        return null;
+    }    
+}
