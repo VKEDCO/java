cd9d08df5053c56353e53256907af7fbbeaebd01
vkedco <vladimir.kulyukin@gmail.com>
2015-07-24 14:14:09 -0600

    Update Partition.java

--- stewarts_calc/org.vkedco.calc.utils/Partition.java
+++ stewarts_calc/org.vkedco.calc.utils/Partition.java
@@ -20,4 +20,18 @@ public class Partition {
         }
         return interval;
     }
+    
+    public static double norm(double[] partition) {
+       if ( partition.length < 2 ) return 0;
+       
+       int i;
+       double norm = partition[1] - partition[0];
+       double curr_norm = 0;
+       for(i = 1; i < partition.length-1; i++) {
+            curr_norm = partition[i+1] - partition[i];
+            if (curr_norm > norm)
+                norm = curr_norm;
+       }
+       return norm; 
+    }
 }
