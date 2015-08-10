bd8fc4ba46a2fa86d48a3d8f91d305dc6d535057
vkedco <vladimir.kulyukin@gmail.com>
2015-08-10 15:36:48 -0600

    Create Piano88A440NoteFrequencyRanges.java

---
+++ tolstov/org.vkedco.audio.music.notes/Piano88A440NoteFrequencyRanges.java
@@ -0,0 +1,61 @@
+package org.vkedco.audio.music.notes;
+
+/**
+ *
+ * @author vladimir kulyukin
+ */
+public class Piano88A440NoteFrequencyRanges {
+    
+    // standard frequency of LA0-A0 is 27.56Hz
+    public static final int LA0_A0_FREQ_RANGE_START = 25;
+    public static final int LA0_A0_FREQ_RANGE_END   = 29;
+    
+    // standard frequency of SI0-B0 is 30.8677Hz
+    public static final int SI0_B0_FREQ_RANGE_START = 30;
+    public static final int SI0_B0_FREQ_RANGE_END   = 32;
+    
+    // standard frequency of DO4-C4 is 261.626Hz
+    public static final int DO4_C4_FREQ_RANGE_START = 258;
+    public static final int DO4_C4_FREQ_RANGE_END   = 263;
+    
+    // standard frequency of RE4-D4 is 293.665Hz
+    public static final int RE4_D4_FREQ_RANGE_START = 291;
+    public static final int RE4_D4_FREQ_RANGE_END   = 296;
+    
+    // standard frequence of MI4-E4 is 329.628Hz
+    public static final int MI4_E4_FREQ_RANGE_START = 327;
+    public static final int MI4_E4_FREQ_RANGE_END   = 332;
+    
+    // standard frequency of FA4-F4 is 349.228Hz
+    public static final int FA4_F4_FREQ_RANGE_START = 347;
+    public static final int FA4_F4_FREQ_RANGE_END = 352;
+    
+    // standard frequence of SOL4-G4 is 391.995Hz
+    public static final int SOL4_G4_FREQ_RANGE_START = 389;
+    public static final int SOL4_G4_FREQ_RANGE_END   = 394;
+    
+    // standard frequency of LA4-A4 is 440Hz
+    public static final int LA4_A4_FREQ_RANGE_START = 435;
+    public static final int LA4_A4_FREQ_RANGE_END   = 445;
+    
+    // standard frequency of DO5-C5 is 523.251Hz
+    public static final int DO5_C5_FREQ_RANGE_START  = 521;
+    public static final int DO5_C5_FREQ_RANGE_END    = 526;
+    
+    // standard frequency of SOL5-G5 is 783.991Hz
+    public static final int SOL5_G5_FREQ_RANGE_START = 781;
+    public static final int SOL5_G5_FREQ_RANGE_END   = 786;
+    
+    // standard frequency of MI5-E5 is 659.255Hz
+    public static final int MI5_E5_FREQ_RANGE_START  = 657;
+    public static final int MI5_E5_FREQ_RANGE_END    = 662;
+    
+    // standard frequency of SI5-B5 is 987.767Hz
+    public static final int SI5_B5_FREQ_RANGE_START  = 985;
+    public static final int SI5_B5_FREQ_RANGE_END    = 990;
+    
+    // standard frequency of RE6-D6 is 1174.66Hz
+    public static final int RE6_D6_FREQ_RANGE_START  = 1172;
+    public static final int RE6_D6_FREQ_RANGE_END    = 1177;
+    
+}
