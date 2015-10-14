9bf817b6c20530f4b4f98d53b5a1ef76d2488f77
vkedco <vladimir.kulyukin@gmail.com>
2015-10-14 17:21:57 -0600

    Update BoundaryHandler.java

--- haar/org.vkedco.wavelets.ripples/BoundaryHandler.java
+++ haar/org.vkedco.wavelets.ripples/BoundaryHandler.java
@@ -1,48 +1,46 @@
 package org.vkedco.wavelets.ripples;
 
 /**
- **************************************
+ *
  * @author vladimir kulyukin
- **************************************
+ * 
+ * TODO: Test it on the example of signal of lengths 2, 4, and 8
  */
-public class CDF22 {
+public class BoundaryHandler {
     
-    static int get_mid_index(int j, int l) {
-        return ((1 << (j - l)) >> 1) - 1;
+    public static int mirror_wrapup(int start, int end, int i) {
+        if ( start > end ) throw new IllegalArgumentException("Start = " + start + " > End = " + end);  
+        final int n = (end - start + 1);
+        final int real_i = start + i;
+        if ( end >= start ) {
+            if ( real_i >= start && real_i <= end ) {
+                return real_i;
+            }
+            else {
+                if ( real_i > end ) {
+                    return start + (n - ((real_i % n) + 1));
+                }
+                else if ( real_i < start ) {
+                    return start + (Math.abs(real_i+1) % n);
+                }
+            }
+        }
+        return -1;
     }
     
-    static int get_s_end_for_level(int j, int l) {
-        return get_mid_index(j, l);
-    }
-    
-    static int get_d_start_for_level(int j, int l) {
-        return get_mid_index(j, l)+1;
-    }
-    
-    public static double compute_d(double[] signal, int j, int l, int n) {
-        final int start = get_d_start_for_level(j, l);
-        final int end   = signal.length-1;
-        
-        final int two_n         = BoundaryHandler.mirror_wrapup(start, end, 2*n);
-        final int two_n_plus_1  = BoundaryHandler.mirror_wrapup(start, end, 2*n+1);
-        final int two_n_plus_2  = BoundaryHandler.mirror_wrapup(start, end, 2*n+2);
-        
-        return signal[two_n_plus_1] - (signal[two_n] + signal[two_n_plus_2])/2.0;
-    }
-    
-    public static double compute_s(double[] signal, int j, int l, int n) {
-        final int start = 0;
-        final int end   = get_s_end_for_level(j, l);
-        
-        final int two_n         = BoundaryHandler.mirror_wrapup(start, end, 2*n);
-        final double d1         = compute_d(signal, j-1, l-1, n-1);
-        final double d2         = compute_d(signal, j-1, l-1, n);
-        return signal[two_n] + (d1+d2)/4.0;
+    public static void test_mirror_wrapup(int start, int end, int i) {
+        System.out.println(start + ", " + end + ", " + i + " -> "    + BoundaryHandler.mirror_wrapup(start, end, i));
+        //System.out.println(start + ", " + end + ", " + (-i) + " -> " + BoundaryHandler.mirror_wrapup(start, end, -i));
     }
     
     public static void main(String[] args) {
-        System.out.println(get_mid_index(4, 0));
+        //test_mirror_wrapup(2, 3, 0);
+        //test_mirror_wrapup(2, 3, 1);
+        //test_mirror_wrapup(0, 4, -5);
+        System.out.println(4 >> 1);
+        System.out.println(4 >> 2);
+        System.out.println(2 << 1);
+        System.out.println(2 << 2);
     }
-    
     
 }
