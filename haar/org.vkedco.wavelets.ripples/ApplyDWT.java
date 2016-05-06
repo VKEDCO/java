1dfa72ffff6ea900e70bd576486dc2f5fcabed11
vkedco <vladimir.kulyukin@gmail.com>
2016-05-06 12:07:14 -0600

    Update ApplyDWT.java

--- haar/org.vkedco.wavelets.ripples/ApplyDWT.java
+++ haar/org.vkedco.wavelets.ripples/ApplyDWT.java
@@ -1,6 +1,10 @@
 package org.vkedco.wavelets.ripples;
 
 import org.vkedco.wavelets.haar.OneDHaar;
+import org.vkedco.cs.dtw.TwoDCurveComparator;
+import org.vkedco.cs.dtw.DTW;
+import org.vkedco.cs.dtw.SquaredEuclideanSim;
+import org.vkedco.wavelets.utils.Utils;
 
 /**
  *****************************
@@ -113,7 +117,7 @@ public class ApplyDWT {
     }
     
     // keep the fast variations. Given signal's size, n, and number of scales, num_scales, apply DWT for a given
-    // number of scales. filter the s part of the transform. reconstruct the signal from the filtered s values.
+    // number of scales. filter the S part of the transform. reconstruct the signal from the filtered s values.
     // subtract the reconstructed signal from the original signal
     public static void genericKeepFastVarsInSignal(double[] signal, ApplyDWT.DWT dwt, int num_iters) {
         // signal_transform holds the DWT transform of signal_tranform of ApplyDWT.DWT
@@ -168,7 +172,7 @@ public class ApplyDWT {
     // 2. set to zeros stuff outside of the range
     // 3. reconstruct the signal and return
     
-    public static double[] multiresSignalReconstruct(double[] signal_transform, ApplyDWT.DWT dwt, ApplyDWT.COEFF coeff, int num_scales, int scale_num) {
+    public static double[] multiresSignalReconstructFromS(double[] signal_transform, ApplyDWT.DWT dwt, ApplyDWT.COEFF coeff, int num_scales, int scale_num) {
         final int signal_size = signal_transform.length;
         int range_start = 0;
         int range_end = 0;
@@ -201,32 +205,72 @@ public class ApplyDWT {
         return reconstructed_signal;
     }
     
-    /*
-    static void multires_ex_4_4_with_noise_p34(String message, ApplyDWT.DWT dwt, int range_start, int range_end, int signal_size, int num_scales) {
-        double[] noisy_signal = generate_signal_with_noise_ex_4_4_p34(signal_size, 500, 4.0);
+    // this is the same as above but puts reconstructed signal into the second array passed to it as argument.
+    public static void multiresSignalReconstruct(double[] signal_transform, double[] reconstructed_signal, ApplyDWT.DWT dwt, ApplyDWT.COEFF coeff, int num_scales, int scale_num) {
+        final int signal_size = signal_transform.length;
+        int range_start = 0;
+        int range_end = 0;
+        //System.out.println("Signal_size = " + signal_size);
         
-        //CDF44.orderedDWTForNumIters(noisy_signal, num_scales, false);
-        ApplyDWT.forwardDWTForNumIters(noisy_signal, dwt, num_scales);
-        
-        double[] signal_transform = new double[signal_size];
+        switch ( coeff ) {
+            case S: 
+                range_end = ApplyDWT.getSCoeffsEnd(signal_size, scale_num);
+                break;
+            case D:
+                range_start = ApplyDWT.getDCoeffsStart(signal_size, scale_num);
+                range_end = ApplyDWT.getDCoeffsEnd(signal_size, scale_num);
+                //System.out.println("D's start = " + range_start);
+                //System.out.println("D's end   = " + range_end);
+                break;
+            default:
+                throw new IllegalArgumentException("multiresSignalReconstruct: unknown coeff type");
+        }
+       
+        System.arraycopy(signal_transform, 0, reconstructed_signal, 0, signal_size);
         for(int i = 0; i < signal_size; i++) {
-            if ( i >= range_start && i <= range_end ) {
-                signal_transform[i] = noisy_signal[i];
-            }
-            else {
-                signal_transform[i] = 0;
+            if ( i < range_start || i > range_end ) {
+                reconstructed_signal[i] = 0;
             }
         }
         
-        System.out.println("=========================");
-        System.out.println(message);
-        display_signal(signal_transform);
-        System.out.println("Inversed Signal");
-        System.out.println("=========================");
-        //CDF44.orderedInverseDWTForNumIters(signal_transform, num_scales, false);
-        ApplyDWT.inverseDWTForNumIters(signal_transform, dwt, num_scales);
-        display_signal(signal_transform);
-        System.out.println("=========================");
+        ApplyDWT.inverseDWTForNumIters(reconstructed_signal, dwt, num_scales);
     }
-    */
+    
+    // apply the keepFastVarsInSignal and then compute the difference b/w the original signal
+    // and the reconstructed signal.
+    // doesSignalContainFastVars(double[] signal, ApplyDWT.DWT dwt, int num_iters) => double
+    public static double doesSignalContainFastVars(double[] signal, ApplyDWT.DWT dwt, int num_iters) {
+        double[] original_signal = new double[signal.length];
+        System.arraycopy(signal, 0, original_signal, 0, signal.length);
+        genericKeepFastVarsInSignal(signal, dwt, num_iters);
+        //return TwoDCurveComparator.compareTwoDCurve(original_signal, signal, new SquaredEuclideanSim(), DTW.MaxWW);
+        return Utils.computeCorrectedSTD(signal);
+    }
+    
+    // 1. apply DWT for N scales, measure the STD of the D(N-1), D(N-2), ..., D(0), S(0)
+    // 2. compare the stats for the signals with high frequencies and lower frequencies
+    public static void computeSTDsForDWTMultiresCoeffs(double[] signal, ApplyDWT.DWT dwt, int num_iters, boolean display_signal_flag) {
+        forwardDWTForNumIters(signal, dwt, num_iters);
+        double[] reconstructed_signal = null; 
+        final int sig_len = signal.length;
+        int start = 0; int end = 0;
+        
+        for(int scale = 1; scale <= num_iters; scale++) {
+            start = getDCoeffsStart(sig_len, scale);
+            end   = getDCoeffsEnd(sig_len, scale);
+            reconstructed_signal = new double[signal.length];
+            multiresSignalReconstruct(signal, reconstructed_signal, dwt, ApplyDWT.COEFF.D, num_iters, scale);
+            System.out.println("start = " + start + "; end = " + end);
+            System.out.println("STD of D[" + (num_iters - scale) + "] = " + Utils.computeCorrectedSTDInRange(signal, start, end));
+            if ( display_signal_flag ) Utils.displaySignalRange(signal, start, end);
+            multiresSignalReconstruct(signal, reconstructed_signal, dwt, ApplyDWT.COEFF.S, num_iters, scale);
+            start = 0;
+            end   = getSCoeffsEnd(sig_len, scale);
+            System.out.println("start = " + 0 + "; end = " + end);
+            System.out.println("STD of S[" + (num_iters - scale) + "] = " + Utils.computeCorrectedSTDInRange(signal, start, end));
+            if ( display_signal_flag) Utils.displaySignalRange(signal, start, end);
+            System.out.println("*********************");
+        }
+        System.out.println();
+    }
 }
